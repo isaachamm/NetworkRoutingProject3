4740fc6acab9ed0672b1df3b387e90ce30563030
Isaac Hamm <isaachamm4@gmail.com>
2023-02-17 11:28:33 -0700

    Created pqarray implementation and tried to implement Dijkstra's for it. Need to pass data from Dijkstra's to get shortest paths

--- NetworkRoutingSolver.py
+++ NetworkRoutingSolver.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+import math
 
 from CS312Graph import *
 import time
@@ -14,18 +14,40 @@ class PQArray:
             q_array is our queue, where we keep the nodes we need to visit in an unordered manner
     """
 
-    def __init__(self):
-        self.dist = []
-        self.q_array = []
+    def __init__(self, nodes):
+        self.dist = [math.inf] * len(nodes)
 
-    def insert(self, node):
+        # 0 if the node is not in the queue, change to 1 if it is
+        # self.q_array = [0] * len(nodes)
+        self.q_array = set()
+
+    def insert(self, node_index):
+        # self.q_array[node_index] = 1
+        self.q_array.add(node_index)
         return
 
-    def decrease_key(self, node):
+    def decrease_key(self, dist, node_index):
+        self.dist[node_index] = dist
         return
 
     def delete_min(self):
-        return
+
+        # minimum is the index of the lowest node, not it's actual value
+        minimum = 0
+        for i in range(len(self.dist)):
+            # Check to see if it's in the PQ
+            if i in self.q_array:
+                # If in pq, compare the new node's value to the current minimum value. If lower, make curr
+                #   index the new minimum
+                if self.dist[i] < self.dist[minimum]:
+                    minimum = i
+        # temp = self.dist[minimum]
+        # self.q_array[minimum] = 0
+        temp = minimum
+        self.q_array.remove(minimum)
+
+        # TODO: does this return the node or the index to the node? depends on the implementation of the other side
+        return temp
 
 
 class HeapArray:
@@ -37,8 +59,9 @@ class HeapArray:
             q_heap is our queue, where we keep the nodes we need to visit in an ordered manner
             q_pointer keeps track of the indices of nodes in our q_heap so that we can access them quickly
     """
-    def __init__(self):
-        self.dist = []
+
+    def __init__(self, nodes):
+        self.dist = [math.inf] * len(nodes)
         self.q_heap = []
         self.q_pointer = []
 
@@ -84,13 +107,29 @@ class NetworkRoutingSolver:
         # TODO: RUN DIJKSTRA'S TO DETERMINE SHORTEST PATHS.
         #       ALSO, STORE THE RESULTS FOR THE SUBSEQUENT
         #       CALL TO getShortestPath(dest_index)
+        #       Need to make new class members to store cost and shortest path
         # if use_heap:
-            # heap implementation of Dijkstra's
+        # heap implementation of Dijkstra's
         # else:
-            # array implementation of Dijkstra's
+        # array implementation of Dijkstra's
+        pq_array = PQArray.__new__(PQArray)
+        pq_array.__init__(self.network.nodes)
+        pq_array.insert(srcIndex)
+        pq_array.decrease_key(0, srcIndex)
+        prev_array = [None] * len(self.network.nodes)
+        while pq_array.q_array:
+            curr_min_node_index = pq_array.delete_min()
+            curr_node = self.network.nodes[curr_min_node_index]
+            for edge in curr_node.neighbors:
+                if (pq_array.dist[edge.dest.node_id] >
+                        pq_array.dist[curr_min_node_index] + edge.length):
+                    node_index = edge.dest.node_id
+                    new_distance = pq_array.dist[curr_min_node_index] + edge.length
+                    pq_array.decrease_key(new_distance, node_index)
+                    prev_array[edge.dest.node_id] = curr_min_node_index
 
         # Need this call to return the shortest path to the GUI once we find it with Dijkstra's
         # self.getShortestPath(destIndex=srcIndex):
-            
+
         t2 = time.time()
         return (t2 - t1)
